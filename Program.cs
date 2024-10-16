71dc19ccc52b6b44671ff32a9651739bc2371660
beskilstark <ben.eskilstark@gmail.com>
2024-10-16 14:50:47 -0400

    updating descriptions

--- Program.cs
+++ Program.cs
@@ -3,19 +3,14 @@
 using System.Diagnostics;
 using CurrentYear = Year2021;
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Stopwatch timer = new();
-        timer.Start();
-        CurrentYear.Problem4.Solve();
-        timer.Stop();
 
-        // Format and display the TimeSpan value.
-        TimeSpan ts = timer.Elapsed;
-        string elapsedTime = String.Format("{0:00}hr {1:00}:{2:00}.{3:000}",
-            ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
-        Console.WriteLine("Finished in " + elapsedTime);
-    }
-}
+Stopwatch timer = new();
+timer.Start();
+CurrentYear.Problem4.Solve();
+timer.Stop();
+
+// Format and display the TimeSpan value.
+TimeSpan ts = timer.Elapsed;
+string elapsedTime = String.Format("{0:00}hr {1:00}:{2:00}.{3:000}",
+    ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+Console.WriteLine("Finished in " + elapsedTime);
